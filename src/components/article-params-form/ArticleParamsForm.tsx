40119f67703fdbee1add743ab0ee0bc217e443c5
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-03-10 22:55:21 +0100

    Реализовала открытие и закрытие формы

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,43 @@
+import { useCallback, useState, useRef, useEffect } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const wrapperRef = useRef<HTMLDivElement>(null);
+
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+
+	const toggleForm = useCallback(() => {
+		setIsOpen(!isOpen);
+	}, [isOpen, setIsOpen]);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				isOpen &&
+				wrapperRef.current &&
+				!wrapperRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		document.addEventListener('mousedown', handleClickOutside);
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen, setIsOpen]);
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+		<div ref={wrapperRef}>
+			<ArrowButton isOpen={isOpen} onClick={toggleForm} />
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
@@ -15,6 +45,6 @@ export const ArticleParamsForm = () => {
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
