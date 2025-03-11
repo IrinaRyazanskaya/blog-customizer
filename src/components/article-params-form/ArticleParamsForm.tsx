63994034f135a98e4481d7ab69a9a8553d8ebc8d
Irina Ryazanskaya <ryazanskaya.irina@yandex.com>
2025-03-11 21:36:48 +0100

    Собрала форму из компонентов

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,8 +1,19 @@
 import { useCallback, useState, useRef, useEffect } from 'react';
 import clsx from 'clsx';
 
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
 
 import styles from './ArticleParamsForm.module.scss';
 
@@ -33,12 +44,61 @@ export const ArticleParamsForm = () => {
 		};
 	}, [isOpen, setIsOpen]);
 
+	const [selectedFontFamilyOption, setSelectedFontFamilyOption] = useState(
+		fontFamilyOptions[0]
+	);
+	const [selectedFontSizeOption, setSelectedFontSizeOption] = useState(
+		fontSizeOptions[0]
+	);
+	const [selectedFontColor, setSelectedFontColor] = useState(fontColors[0]);
+	const [selectedBackgroundColor, setSelectedBackgroundColor] = useState(
+		backgroundColors[0]
+	);
+	const [selectedContentWidth, setSelectedContentWidth] = useState(
+		contentWidthArr[0]
+	);
+
 	return (
 		<div ref={wrapperRef}>
 			<ArrowButton isOpen={isOpen} onClick={toggleForm} />
 			<aside
 				className={clsx(styles.container, isOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text uppercase size={31} weight={800}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={selectedFontFamilyOption}
+						onChange={setSelectedFontFamilyOption}
+						options={fontFamilyOptions}
+						title='Шрифт'
+					/>
+					<RadioGroup
+						selected={selectedFontSizeOption}
+						name='radio'
+						onChange={setSelectedFontSizeOption}
+						options={fontSizeOptions}
+						title='Размер шрифта'
+					/>
+					<Select
+						selected={selectedFontColor}
+						onChange={setSelectedFontColor}
+						options={fontColors}
+						title='Цвет шрифта'
+					/>
+					<Separator />
+					<Select
+						selected={selectedBackgroundColor}
+						onChange={setSelectedBackgroundColor}
+						options={backgroundColors}
+						title='Цвет фона'
+					/>
+					<Select
+						selected={selectedContentWidth}
+						onChange={setSelectedContentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
