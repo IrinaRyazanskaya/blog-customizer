9c571102134da54e55ca28af755627fcc8fb4f5f
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-10-19 15:54:58 +0000

    Применила новые настройки форматирования

--- src/index.tsx
+++ src/index.tsx
@@ -1,44 +1,41 @@
-import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties, useState } from 'react';
+import { createRoot } from "react-dom/client";
+import { StrictMode, CSSProperties, useState } from "react";
 
-import { Article } from './components/article/Article';
-import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
-import type { ArticleStateType } from './constants/articleProps';
+import { Article } from "./components/article/Article";
+import { ArticleParamsForm } from "./components/article-params-form/ArticleParamsForm";
+import { defaultArticleState } from "./constants/articleProps";
+import type { ArticleStateType } from "./constants/articleProps";
 
-import './styles/index.scss';
-import styles from './styles/index.module.scss';
+import "./styles/index.scss";
+import styles from "./styles/index.module.scss";
 
-const domNode = document.getElementById('root') as HTMLDivElement;
+const domNode = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
-	const [articleState, setArticleState] =
-		useState<ArticleStateType>(defaultArticleState);
+  const [articleState, setArticleState] = useState<ArticleStateType>(defaultArticleState);
 
-	return (
-		<main
-			className={styles.main}
-			style={
-				{
-					'--font-family': articleState.fontFamilyOption.value,
-					'--font-size': articleState.fontSizeOption.value,
-					'--font-color': articleState.fontColor.value,
-					'--container-width': articleState.contentWidth.value,
-					'--bg-color': articleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm
-				defaults={defaultArticleState}
-				setArticleState={setArticleState}
-			/>
-			<Article />
-		</main>
-	);
+  return (
+    <main
+      className={styles.main}
+      style={
+        {
+          "--font-family": articleState.fontFamilyOption.value,
+          "--font-size": articleState.fontSizeOption.value,
+          "--font-color": articleState.fontColor.value,
+          "--container-width": articleState.contentWidth.value,
+          "--bg-color": articleState.backgroundColor.value,
+        } as CSSProperties
+      }
+    >
+      <ArticleParamsForm defaults={defaultArticleState} setArticleState={setArticleState} />
+      <Article />
+    </main>
+  );
 };
 
 root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
