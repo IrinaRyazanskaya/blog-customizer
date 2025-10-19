06477b55c936a150d205922a3c15c9e65f5fb7c0
IrinaRyazanskaya <ryazanskaya.irina@yandex.com>
2025-10-19 18:24:53 +0000

    Отрефакторила вебпак конфиги

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,30 +1,30 @@
-const HTMLWebpackPlugins = require("html-webpack-plugin");
+const path = require("path");
+
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path"); //для того чтобы превратить отнсительный путь в абсолютный мы будем использовать пакет path
 const webpack = require("webpack");
 
 const production = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: path.resolve(__dirname, "..", "./src/index.tsx"), //точка входа в наше приложение содержит абсолютный путь к index.ts
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+
   output: {
-    path: path.resolve(__dirname, "..", "./dist"), //путь куда будет собираться наш проект
-    filename: production ? "static/scripts/[name].[contenthash].js" : "static/scripts/[name].js", // имя нашего бандла
     publicPath: "auto",
+    path: path.resolve(__dirname, "..", "./dist"),
+    filename: production ? "static/scripts/[name].[contenthash].js" : "static/scripts/[name].js",
   },
-  //Нужно помочь вебпаку научится работать с jsx и tsx файлами для этого используют ts loader
+
   module: {
     rules: [
       {
-        test: /\.[tj]sx?$/, //содержит регулярное выражение, которое содержит информацию какие файлы должны обрабатываться этим loader'ом
+        test: /\.[tj]sx?$/,
         use: [
           {
             loader: "ts-loader",
           },
-        ], // для того чтобы ts-loader корректно отработал нам нужен tsconfig его можно создать вручную, а можно создать автоматически
-        /** чтобы проиницилизовать его автоматически можно установить пакет typesctipt глобально или использовать npx выполнив команду npx tsc --init
-				После создания конфига нужно включить "allowJs": true, чтобы работать не только c typescript, также меняем "jsx": "react" чтобы мы могли работать с react компонентами и включаем карту ресурсов "sourceMap": true, пока на этом все вернемся в этот конфиг позже*/
+        ],
         exclude: /node_modules/,
       },
       {
@@ -61,7 +61,7 @@ module.exports = {
                 namedExport: false,
                 exportLocalsConvention: "as-is",
               },
-              importLoaders: 2, //Значение 2 говорит о том, что некоторые трансформации PostCSS нужно применить до css-loader.
+              importLoaders: 2,
             },
           },
           "postcss-loader",
@@ -75,16 +75,18 @@ module.exports = {
       },
     ],
   },
+
   resolve: {
-    extensions: [".js", ".jsx", ".tsx", ".ts", ".json"], //указываем файлы с которыми будет работать webpack
+    extensions: [".js", ".jsx", ".tsx", ".ts", ".json"],
     alias: {
+      components: path.resolve(__dirname, "..", "./src/components"),
       fonts: path.resolve(__dirname, "..", "./src/fonts"),
       src: path.resolve(__dirname, "..", "./src"),
-      components: path.resolve(__dirname, "..", "./src/components"),
     },
   },
+
   plugins: [
-    new HTMLWebpackPlugins({
+    new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "..", "./public/index.html"),
     }),
     new CleanWebpackPlugin(),
@@ -92,7 +94,7 @@ module.exports = {
       filename: production ? "static/styles/[name].[contenthash].css" : "static/styles/[name].css",
     }),
     new webpack.EnvironmentPlugin({
-      NODE_ENV: "development", // значение по умолчанию 'development' если переменная process.env.NODE_ENV не передана
+      NODE_ENV: "development",
     }),
   ],
 };
